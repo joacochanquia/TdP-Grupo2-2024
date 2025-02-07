0cf787dfd54c277ba7950997652d0dbc3d6c2590
joacochanquia <joaquin.chanquia28@gmail.com>
2025-02-07 16:08:54 -0300

    pruebasDeDibujo

--- firmware_v3/PRUEBAS/PruebaMotores/src/PruebaMotores.c
+++ firmware_v3/PRUEBAS/PruebaMotores/src/PruebaMotores.c
@@ -1,4 +1,6 @@
 #include "sapi.h"  // Biblioteca de la EDU-CIAA
+#include <math.h>
+
 
 // Definir los pines para control del motor paso a paso
 #define STEP_PIN1 GPIO7  // GPIO1 para STEP
@@ -8,7 +10,10 @@
 #define DIR_PIN2 GPIO3  // GPIO3 para Direcci�n (DIR)
 #define EN_PIN2 LCD1
 
+#define SPEED 5
 #define SMOOTHNESS 10
+
+#define PI 3.1415926535897932384626433
 
 //Definir el sentido de giro de los motores
 #define H true
@@ -19,6 +24,17 @@
 #define LEFT false
 
 #define SERVO_N   SERVO3
+#define SERVO_UP  70
+#define SERVO_DOWN 45
+
+#define MAX_POS_X 282
+#define MIN_POS_X 15
+#define MAX_POS_Y 195
+#define MIN_POS_Y 15
+
+int posX;
+int posY;
+
 
 typedef struct {
    int EN;
@@ -27,8 +43,24 @@ typedef struct {
 } MotorPins_t;
    
 
+void initServo( int servoPin ){
+   // Configurar Servo
+   servoConfig( 0, SERVO_ENABLE );
+   servoConfig( servoPin, SERVO_ENABLE_OUTPUT );
+}
+
+void servoUp( int servoPin ){
+   servoWrite( servoPin, SERVO_UP );
+   delay(200);
+}
+
+void servoDown( int servoPin ){
+   servoWrite( servoPin, SERVO_DOWN );
+   delay(200);
+}
+
 // Funcion de inicializacion de los motores
-void MotorInit(MotorPins_t * motor, int enPin, int dirPin, int stepPin) {
+void motorInit(MotorPins_t * motor, int enPin, int dirPin, int stepPin) {
    
    motor->EN = enPin;
    motor->DIR = dirPin;
@@ -43,8 +75,22 @@ void MotorInit(MotorPins_t * motor, int enPin, int dirPin, int stepPin) {
    
 }
 
+void printerInit( MotorPins_t * motor1, MotorPins_t * motor2 ){
+
+   motorInit( motor1, EN_PIN1, DIR_PIN1, STEP_PIN1 );
+   motorInit( motor2, EN_PIN2, DIR_PIN2, STEP_PIN2 );
+
+   posX = 100;
+   posY = 100;
+
+   initServo( SERVO_N );
+   servoUp( SERVO_N );
+
+}
+
 // Funcion para realizar un paso en un motor
-void MotorStep(MotorPins_t motor, bool direction) {
+// NO INCLUYE EL ENABLE
+void motorStep(MotorPins_t motor, bool direction) {
     // Configurar la direcci�n del motor
     if (direction) {
         gpioWrite(motor.DIR, ON);  // Sentido horario
@@ -59,8 +105,20 @@ void MotorStep(MotorPins_t motor, bool direction) {
     delay(1);
 }
 
-//Funcion para realizar un paso en dos motores en simultaneo
-void TwoMotorStep(MotorPins_t motor1, bool direction1, MotorPins_t motor2, bool direction2) {
+// Funcion para realizar varios pasos en un motor
+void motorRun(MotorPins_t motor, bool direction, int stepCount, int velocity){
+   gpioWrite(motor.EN, OFF);
+   delay(1);
+   for (int i = 0; i < stepCount; i++) {
+      motorStep(motor, direction);
+      delay(velocity);
+   }
+   gpioWrite(motor.EN, ON);
+}
+
+// Funcion para realizar un paso en dos motores en simultaneo
+// NO INCLUYE EL ENABLE
+void twoMotorStep(MotorPins_t motor1, bool direction1, MotorPins_t motor2, bool direction2) {
     // Configurar la direcci�n del motor
     if (direction1) {
         gpioWrite(motor1.DIR, ON);  // Sentido horario
@@ -82,81 +140,148 @@ void TwoMotorStep(MotorPins_t motor1, bool direction1, MotorPins_t motor2, bool 
     delay(1);
 }
 
-// Funcion para realizar varios pasos en un motor
-void MotorRun(MotorPins_t motor, bool direction, int stepCount, int velocity){
-   gpioWrite(motor.EN, OFF);
-   delay(1);
-   for (int i = 0; i < stepCount; i++) {
-      MotorStep(motor, direction);
-      delay(velocity);
-   }
-   gpioWrite(motor.EN, ON);
-}
-
 // Funcion para realizar varios pasos en dos motores en simultaneo
-void TwoMotorRun(MotorPins_t motor1, bool direction1, MotorPins_t motor2, bool direction2, int stepCount, int velocity){
+void twoMotorRun(MotorPins_t motor1, bool direction1, MotorPins_t motor2, bool direction2, int stepCount, int velocity){
    gpioWrite(motor1.EN, OFF);
    gpioWrite(motor2.EN, OFF);
    delay(1);
    for (int i = 0; i < stepCount; i++) {
-      TwoMotorStep(motor1, direction1, motor2, direction2);
+      twoMotorStep(motor1, direction1, motor2, direction2);
       delay(velocity);
    }
    gpioWrite(motor1.EN, ON);
    gpioWrite(motor2.EN, ON);
 }
 
+int insideXlimits( int cant , int sentido){
+   /*int posFin = posX + (cant * ( sentido ? 1 : -1 ));
+   if (( posFin < MAX_POS_X )&&( posFin > MIN_POS_X )){
+      return 1;
+   }else{
+      return 0;
+   }*/
+   return 1;
+}
+
+// Funcion para mover un paso en el eje X
+// NO INCLUYE EL ENABLE
+void stepMoveX(MotorPins_t motor1, MotorPins_t motor2, bool sentido){
+   /*if ( insideXlimits( 1 , sentido ) ){
+      twoMotorStep(motor1, sentido, motor2, sentido);
+      posX += ( sentido ? 1 : -1 );
+   }*/
+   twoMotorStep(motor1, sentido, motor2, sentido);
+}
+
 // Funcion para mover en el eje X
 // Ambos motores deben moverse en el mismo sentido para realizar el movimiento
-void MoveX(MotorPins_t motor1, MotorPins_t motor2, bool sentido, int stepCount, int velocity){
-   TwoMotorRun(motor1, sentido, motor2, sentido, stepCount, velocity);
+void moveX(MotorPins_t motor1, MotorPins_t motor2, bool sentido, int stepCount, int velocity){
+   /*if ( insideXlimits( stepCount , sentido ) ){
+      twoMotorRun(motor1, sentido, motor2, sentido, stepCount, velocity);
+      posX += ( sentido ? stepCount : -stepCount );
+   }*/
+   twoMotorRun(motor1, sentido, motor2, sentido, stepCount, velocity);
+}
+
+int insideYlimits( int cant , int sentido){
+   /*int posFin = posY + (cant * ( sentido ? 1 : -1 ));
+   if (( posFin < MAX_POS_Y )&&( posFin > MIN_POS_Y )){
+      return 1;
+   }else{
+      return 0;
+   }*/
+   return 1;
+}
+
+// Funcion para mover un paso en el eje Y
+// NO INCLUYE EL ENABLE
+void stepMoveY(MotorPins_t motor1, MotorPins_t motor2, bool sentido){
+   /*if ( insideYlimits( 1 , sentido ) ){
+      twoMotorStep(motor1, !sentido, motor2, !sentido);
+      posY += ( sentido ? 1 : -1 );
+   }*/
+   twoMotorStep(motor1, sentido, motor2, !sentido);
 }
 
 // Funcion para mover en el eje Y
 // Ambos motores deben moverse en sentidos opuestos para realizar el movimiento
-void MoveY(MotorPins_t motor1, MotorPins_t motor2, bool sentido, int stepCount, int velocity){
-   TwoMotorRun(motor1, sentido, motor2, !sentido, stepCount, velocity);
+void moveY(MotorPins_t motor1, MotorPins_t motor2, bool sentido, int stepCount, int velocity){
+   /*if ( insideYlimits( stepCount , sentido ) ){
+      twoMotorRun(motor1, !sentido, motor2, !sentido, stepCount, velocity);
+      posY += ( sentido ? stepCount : -stepCount );
+   }*/
+   twoMotorRun(motor1, sentido, motor2, !sentido, stepCount, velocity);
+}
+
+
+void moveDiagonal(MotorPins_t motor1, MotorPins_t motor2, bool sentidoX, bool sentidoY, int stepCount, int velocity){
+   if ( sentidoX ^ sentidoY ) {
+      motorRun(motor1, !sentidoX, stepCount, velocity);
+   }else{
+      motorRun(motor2, !sentidoX, stepCount, velocity);
+   }
 }
 
 // Funcion para dibujar un cuadrado
 void drawSquare(MotorPins_t motor1, MotorPins_t motor2, int side, int velocity){
-   MoveX(motor1, motor2, RIGHT, side, velocity);
-   MoveY(motor1, motor2, DOWN, side, velocity);
-   MoveX(motor1, motor2, LEFT, side, velocity);
-   MoveY(motor1, motor2, UP, side, velocity);
+   moveX(motor1, motor2, RIGHT, side, velocity);
+   moveY(motor1, motor2, DOWN, side, velocity);
+   moveX(motor1, motor2, LEFT, side, velocity);
+   moveY(motor1, motor2, UP, side, velocity);
 }
 
 // Funcion para dibujar un rectangulo
 void drawRectangle(MotorPins_t motor1, MotorPins_t motor2, int side1, int side2, int velocity){
-   MoveX(motor1, motor2, RIGHT, side1, velocity);
-   MoveY(motor1, motor2, DOWN, side2, velocity);
-   MoveX(motor1, motor2, LEFT, side1, velocity);
-   MoveY(motor1, motor2, UP, side2, velocity);
+   moveX(motor1, motor2, RIGHT, side1, velocity);
+   moveY(motor1, motor2, DOWN, side2, velocity);
+   moveX(motor1, motor2, LEFT, side1, velocity);
+   moveY(motor1, motor2, UP, side2, velocity);
 }
- 
+
+// Funcion para dibujar un circulo
+void drawCircle(MotorPins_t motor1, MotorPins_t motor2, int radius, int velocity) {
+   int steps = 360; // Dividir el círculo en 360 pasos
+   for (int i = 0; i < steps; i++) {
+      float angle = (2 * PI * i) / steps;
+      int x = radius * cos(angle);
+      int y = radius * sin(angle);
+      
+      // Mover en el eje X
+      moveX(motor1, motor2, x > 0 ? RIGHT : LEFT, fabs(x), velocity);
+      
+      // Mover en el eje Y
+      moveY(motor1, motor2, y > 0 ? UP : DOWN, fabs(y), velocity);
+   }
+}
+
+void drawFigure( char c ){
+   int i;
+   switch(c)
+   {
+      case 'C':
+         drawSquare(motor1, motor2, 80, SPEED);
+         break;
+      case 'O':
+         drawCircle(motor1, motor2, 2, SPEED);
+         break;
+      default :
+         for(i=0;i<10;i++){
+            gpioWrite( LEDR, ON );
+            delay(200);
+            gpioWrite( LEDR, OFF );
+            delay(200);
+         }
+   }
+}
 
 // Funcion principal del programa
 int main(void) {
    boardConfig();
    
-   bool_t valor = 0;
-
-   uint8_t servoAngle = 0; // 0 a 180 grados
-
-   // Configurar Servo
-   valor = servoConfig( 0, SERVO_ENABLE );
-
-   valor = servoConfig( SERVO_N, SERVO_ENABLE_OUTPUT );
-
-   // Usar Servo
-   valor = servoWrite( SERVO_N, servoAngle );
-   servoAngle = servoRead( SERVO_N );
-   
    MotorPins_t motor1;
-   MotorInit(&motor1, EN_PIN1, DIR_PIN1, STEP_PIN1);
-   
    MotorPins_t motor2;
-   MotorInit(&motor2, EN_PIN2, DIR_PIN2, STEP_PIN2);
+
+   printerInit( &motor1, &motor2 );
    
     // Bucle infinito para controlar el motor
     while (1) {
@@ -169,17 +294,26 @@ int main(void) {
       gpioWrite( LEDB, OFF );
 
       delay(500);
-       
-       
+      
+
       gpioWrite( LED2, ON );
-      drawSquare(motor1, motor2, 200, 5);
+      servoDown( SERVO_N );
+      drawCircle(motor1, motor2, 2, SPEED);
+      servoUp( SERVO_N );
+      gpioWrite( LED2, OFF ); 
+
+/*
+      gpioWrite( LED2, ON );
+      servoDown( SERVO_N );
+      drawSquare(motor1, motor2, 80, SPEED);
+      servoUp( SERVO_N );
       gpioWrite( LED2, OFF );
-       
+*/
 /*
       gpioWrite( LEDR, ON );
       gpioWrite( LED3, ON );
       // Realizar 200 pasos en sentido horario motor 1
-      MotorRun(motor1, H, 200, 5);
+      motorRun(motor1, H, 200, SPEED);
       gpioWrite( LEDR, OFF );
       gpioWrite( LED3, OFF );
 
@@ -189,7 +323,7 @@ int main(void) {
       gpioWrite( LEDG, ON );
       gpioWrite( LED3, ON );
       // Realizar 200 pasos en sentido horario motor 2
-      MotorRun(motor2, H, 200, 5);
+      motorRun(motor2, H, 200, SPEED);
       gpioWrite( LEDG, OFF );
       gpioWrite( LED3, OFF );
 
@@ -199,7 +333,7 @@ int main(void) {
       gpioWrite( LEDR, ON );
       gpioWrite( LED1, ON );
       // Realizar 200 pasos en sentido antihorario motor 1
-      MotorRun(motor1, AH, 200, 5);
+      motorRun(motor1, AH, 200, SPEED);
       gpioWrite( LEDR, OFF );
       gpioWrite( LED1, OFF );
 
@@ -209,7 +343,7 @@ int main(void) {
       gpioWrite( LEDG, ON );
       gpioWrite( LED1, ON );
       // Realizar 200 pasos en sentido antihorario motor 2
-      MotorRun(motor2, AH, 200, 5);
+      motorRun(motor2, AH, 200, SPEED);
       gpioWrite( LEDG, OFF );
       gpioWrite( LED1, OFF );
 
@@ -217,7 +351,7 @@ int main(void) {
       delay(1000);
 
       gpioWrite( LED2, ON );
-      TwoMotorRun(motor1, H, motor2, H, 200, 5);
+      twoMotorRun(motor1, H, motor2, H, 200, SPEED);
       gpioWrite( LED2, OFF );
 */
       delay(1000);
